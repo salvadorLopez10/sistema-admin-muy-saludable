e45b877a02ad86d5ac37ff6a640ef0d08ffe9070
José Salvador Lopez Balleza <jslb_cafcb10@hotmail.com>
2025-08-27 19:13:13 -0600

    Fix: Se establece la url base

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,20 @@ import { Sidebar } from './components/Sidebar';
 import { AppRoutes } from './routes/AppRoutes';
 import './index.css';
 
+// Función para obtener el basename según el ambiente
+const getBasename = () => {
+  const env = import.meta.env.VITE_APP_ENV;
+  console.log('Entorno actual (VITE_APP_ENV):', env);
+  if (env === 'production') return '/sistema_admin';
+  if (env === 'development') return '/sistema_admin_qa';
+  return ''; // Para desarrollo local (localhost)
+};
+
+
 function App() {
   return (
     <AuthProvider>
-      <Router>
+      <Router basename={getBasename()}>
         <ProtectedRoute>
           <div className="flex h-screen bg-gray-100">
             <Sidebar />
